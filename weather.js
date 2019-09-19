bb970f750a788b09f2e5b238d66540e2de32e5ee
Trai Nguyen <tnguyen@at-pac.com>
2019-09-19 09:56:03 -0400

    added js style and html to weather api

---
+++ weather.js
@@ -0,0 +1,32 @@
+var inputCity = "";
+var weatherAPI = "";
+
+function displayWeather () {
+    $.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity},us&units=imperial&appid=a14b5cd39920b60f089d913e4253e5f5`, 
+        function(data){
+            console.log(data);
+
+            let icon = `http://api.openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            $(".icon").attr("src", icon);
+            $(".city").html(data.name);
+            $(".description").html(data.weather[0].description);
+            $(".temp").html(`${Math.floor(data.main.temp)} °F`)
+        }
+    );
+};
+// when submit button is pressed
+$("#submit").click(function() {
+    inputCity = $("#cityInput").val();
+    weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity},us&units=imperial&appid=a14b5cd39920b60f089d913e4253e5f5`;
+    displayWeather();
+});
+// when the enter key is pressed
+$('#cityInput').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        inputCity = $("#cityInput").val();
+        weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity},us&units=imperial&appid=a14b5cd39920b60f089d913e4253e5f5`;
+        displayWeather();
+    }
+});
+
